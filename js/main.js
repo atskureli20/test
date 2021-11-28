62f4ccf0cc23faf65abfe88d18f014b539fc53fa
Vazha Atskureli <70076435+atskureli20@users.noreply.github.com>
2021-11-28 15:07:01 +0400

    Test

---
+++ js/main.js
@@ -0,0 +1,75 @@
+ 
+    $(document).ready(function(){
+        $('.carousel-slider').slick({
+            centerMode: true,
+            dots: true,
+            infinite: true,
+            speed: 300,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            slidesToShow: 1,
+            adaptiveHeight: true,
+            fade: true,
+            arrows: true,
+            prevArrow: '<span class="priv-arrow"><i class="fas fa-chevron-left"></i></span>',
+            nextArrow: '<span class="next-arrow"><i class="fas fa-chevron-right"></i></span>',
+            cssEase: 'linear'
+        });
+        $('.f-box-wraper').slick({
+            lazyLoad: 'ondemand',
+            centerMode: true,
+            slidesToShow: 2,
+            speed: 300,
+            autoplaySpeed: 4000,
+            arrows: false,
+            slidesToScroll: 1,
+            variableWidth: true
+        });
+        
+        // new WOW().init();
+        // $('[data-toggle="counter-up"]').counterUp({
+        //     delay: 10,
+        //     time: 1000
+        // });
+        $("a").on('click', function(event) {
+            if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function(){
+                window.location.hash = hash;
+            });
+            }
+        });
+    });
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 450) {
+            $(".arrow-wraper-pos-hide").addClass("arrow-wraper-pos-show");
+        } else {
+            $('.arrow-wraper-pos-hide').removeClass('arrow-wraper-pos-show');
+        }
+    });
+ 
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navigation');
+    const navLinks = document.querySelectorAll('.navigation li');
+    const searchbg = document.querySelector('.search__wraper');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        searchbg.classList.toggle('search__wraper__bg');
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                linl.style.animation = '';
+            } else {
+                link.style.animation = 'navLinkFade 0.5s ease forwards $(index / 7 + 0.5s}s'
+            }
+        });
+        
+        burger.classList.toggle('toggle');
+    });
+};
+navSlide();
+ 
